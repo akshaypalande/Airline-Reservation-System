38df828b37c59d0db8bc927eff559c5b5b3c60bf
akshaypalande <59253318+akshaypalande@users.noreply.github.com>
2022-06-21 08:11:51 +0530

    [Akshay] add: added UC1

---
+++ Airline.py
@@ -0,0 +1,27 @@
+
+from UC import AirMain
+
+class Bookings():
+    
+    def __init__(self, first_name, last_name, address, from_city, to_city, seat, phone_number, email):
+        """
+            Description: Constructor of Bookings Class
+            Parametres: Takes class fields
+            Retuns: None, Just initialize the values of object
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.from_city = from_city
+        self.to_city = to_city
+        self.seat = seat
+        self.phone_number = phone_number
+        self.email = email
+
+    def __str__(self):
+        """
+            Description: To return textual content of the Booking class Object
+            Parameters: Takes Instance (Object) of class as arguments
+            Returns: Returns String Representation of object, understandable by User
+        """
+        return f"Full Name is {self.first_name} {self.last_name}\ntravelling details address is {self.address},{self.from_city},{self.to_city},{self.seat}\nPhone Number & email is {self.phone_number} and {self.email} respectively"
